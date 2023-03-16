7051e09df7f713880814cc07d53a191865ad3e5d
Nate Brown <nathan.d.brown@gmail.com>
2023-03-15 19:58:14 -0700

    added profiles

--- src/About.js
+++ src/About.js
@@ -1,10 +1,21 @@
 import { Component } from "react";
+import trey from './img/trey.jpeg';
+import nate from './img/nate.jpeg';
 
 class About extends Component {
 
   render() {
-    /* TODO: render information about the developers */
-    return <p>About Us page coming soon</p>
+    return (
+      <>
+        <h2>About The Bookworms</h2>
+        <h4>Bookworm #1: Trey Young</h4>
+        <img src={trey} alt="Bookworm Trey" />
+        <p><a href="https://github.com/goodpudding">James Young(Goodpudding) @ GitHub</a></p>
+        <h4>Bookworm #2: Nate Brown</h4>
+        <img src={nate} alt="Bookworm Nate" />
+        <p><a href="https://github.com/nate-brown-1">Nate Brown @ GitHub</a></p>
+      </>
+  )
   }
 };
 
